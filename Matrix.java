0a5c5707e674eee4ff68f46b5a42751e0fbc5ade
Corwin Eisenbeiss <chimkingang@Corwins-MacBook-Air.local>
2025-10-24 16:07:55 -0400

    Added elemntary row operations

--- Matrix.java
+++ Matrix.java
@@ -15,5 +15,23 @@ public class Matrix{
     contents=mat;
   }
 
+  private void elem1(int a, int b){
+    double[] k=contents[a];
+    contents[a]=contents[b];
+    contents[b]=k;
+  }
+
+  private void elem2(int a, int b, double k){
+    for(int c=0; c<contents[a]; c++){
+      contents[b][c]+=k*contents[a][c];
+    }
+  }
+
+  private void elem3(int a, double k){
+    for(int c=0; c<contents[a]; c++){
+      contents[a][c]*=k;
+    }
+  }
+
 
 }
